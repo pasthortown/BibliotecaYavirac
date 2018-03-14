221516f2f6e95b20ac32e9efcbd52ed5d2dccae0
oscarin007 <36011735+oscarin007@users.noreply.github.com>
2018-03-14 17:09:45 -0500

    actualizacion

--- client/src/app/layout/CRUD/recurso/recurso.module.ts
+++ client/src/app/layout/CRUD/recurso/recurso.module.ts
@@ -7,6 +7,11 @@ import { RecursoComponent } from './recurso.component';
 import { RecursoService } from './recurso.service';
 
 import { TipoRecursoService } from '../tiporecurso/tiporecurso.service';
+import { AutorService } from '../autor/autor.service';
+import { CategoriaRecursoService } from '../categoriarecurso/categoriarecurso.service';
+import { ProductoraService } from '../productora/productora.service';
+import { EstadoRecursoService } from '../estadorecurso/estadorecurso.service';
+
 
 @NgModule({
    imports: [
