325af9f11e6d8e8e2095bd86109c3c75f76034e9
pasthortown <pastorchessmaster@gmail.com>
2018-03-19 16:19:24 -0500

    cambios

--- BDD/HECTOR/Autor.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `Autor` (`id`,`nombres`,`apellidos`,`urlWeb`) VALUES (1,'Daniel','Florian','http://www.floriandaniel.it/');
-INSERT INTO `Autor` (`id`,`nombres`,`apellidos`,`urlWeb`) VALUES (2,'Roel J.','Wieringa','http://wwwhome.ewi.utwente.nl/~roelw/');
-INSERT INTO `Autor` (`id`,`nombres`,`apellidos`,`urlWeb`) VALUES (3,'Marlon ','Dumas','https://kodu.ut.ee/~dumas/');
-INSERT INTO `Autor` (`id`,`nombres`,`apellidos`,`urlWeb`) VALUES (4,'Yosi','Ben - Asher','http://cs.haifa.ac.il/~yosi/');
-INSERT INTO `Autor` (`id`,`nombres`,`apellidos`,`urlWeb`) VALUES (5,'James T.','Strieb','http://www.jtstreib.com/');
--- BDD/HECTOR/Productora.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `Productora` (`id`,`nombre`,`urlWeb`) VALUES (1,'Springer','http://www.springer.com');
-INSERT INTO `Productora` (`id`,`nombre`,`urlWeb`) VALUES (2,'Cambridge','http://www.cambridge.org/');
-INSERT INTO `Productora` (`id`,`nombre`,`urlWeb`) VALUES (3,'Alfaomega','https://www.alfaomega.com.co/index.php');
