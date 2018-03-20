ace928e0ff552bbfac266fb652f9aeb7d77f1483
pasthortown <pastorchessmaster@gmail.com>
2018-03-20 12:40:06 -0500

    registro recurso

--- client/src/app/layout/registroRecurso/registroRecurso.component.ts
+++ client/src/app/layout/registroRecurso/registroRecurso.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { Productora } from './../../entidades/CRUD/Productora';
+import { Autor } from './../../entidades/CRUD/Autor';
+import { ModalComponent } from './../bs-component/components';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-registroRecurso',
@@ -6,7 +11,41 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./registroRecurso.component.scss']
 })
 export class RegistroRecursoComponent implements OnInit {
-    constructor() {}
+    entidadSeleccionadaAutor: Autor;
+    entidadSeleccionadaProductora: Productora;
 
-    ngOnInit() {}
+    constructor(public toastr: ToastsManager, vcr: ViewContainerRef, private modalService: NgbModal) {
+        this.toastr.setRootViewContainerRef(vcr);
+    }
+
+    ngOnInit() {
+        this.refresh();
+    }
+
+    refresh() {
+        this.entidadSeleccionadaAutor = new Autor();
+        this.entidadSeleccionadaAutor.id = 0;
+        this.entidadSeleccionadaProductora = new Productora();
+        this.entidadSeleccionadaProductora.id = 0;
+    }
+
+    openAutor(content){
+        this.modalService.open(content)
+        .result
+        .then((result => {
+
+        }),(result => {
+           //Esto se ejecuta si la ventana se cierra sin aceptar los cambios
+        }));
+    }
+
+    openProductora(content){
+        this.modalService.open(content)
+        .result
+        .then((result => {
+
+        }),(result => {
+           //Esto se ejecuta si la ventana se cierra sin aceptar los cambios
+        }));
+    }
 }
--- client/src/app/layout/registroRecurso/registroRecurso.module.ts
+++ client/src/app/layout/registroRecurso/registroRecurso.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { NgxBarcodeModule } from 'ngx-barcode';
 import { RegistroRecursoRoutingModule } from './registroRecurso-routing.module';
 import { RegistroRecursoComponent } from './registroRecurso.component';
 
 @NgModule({
-    imports: [CommonModule, RegistroRecursoRoutingModule],
+    imports: [CommonModule,
+              RegistroRecursoRoutingModule,
+              NgxBarcodeModule,
+              NgbModule,
+              FormsModule,
+            ],
     declarations: [RegistroRecursoComponent]
 })
 export class RegistroRecursoModule {}
